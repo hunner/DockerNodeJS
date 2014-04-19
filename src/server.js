5a1262d878a20b4db62d3c191f1cea13fe70ffd7
Rahul Singh <rsingh@distelli.com>
2014-04-19 09:28:53 -0700

    Changed the message

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ var app = express();
 app.use(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World from NodeJS and Distelli!');
+  response.send('Hello World from NodeJS, Docker & Distelli!');
 });
 
 var port = process.env.PORT || 8080;
